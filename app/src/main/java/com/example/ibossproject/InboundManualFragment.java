7d4203d1c7ec76fc1664ad9373d751e9dd51f6ed
zxc2694 <zxc2694>
2020-03-10 21:23:00 +0800

    Add inbound table

--- app/src/main/java/com/example/ibossproject/InboundManualFragment.java
+++ app/src/main/java/com/example/ibossproject/InboundManualFragment.java
@@ -1,18 +1,23 @@
 package com.example.ibossproject;
 
-
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class InboundManualFragment extends Fragment {
-
+    private View mainView;
+    private MainActivity mainActivity;
+    @Override
+    public void onAttach(Context context)
+    {
+        super.onAttach(context);
+        mainActivity = (MainActivity)context;
+    }
 
     public InboundManualFragment() {
         // Required empty public constructor
@@ -23,7 +28,10 @@ public class InboundManualFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inbound_manual, container, false);
+        mainView = inflater.inflate(R.layout.fragment_inbound_manual, container, false);
+
+        return mainView;
+
     }
 
 }
