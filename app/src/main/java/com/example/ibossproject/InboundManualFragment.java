f7f05fff29091f7540bad93426ddc0b0574bf0ab
zxc2694 <zxc2694>
2020-03-10 22:34:25 +0800

    inbound add_button function test - ok

--- app/src/main/java/com/example/ibossproject/InboundManualFragment.java
+++ app/src/main/java/com/example/ibossproject/InboundManualFragment.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class InboundManualFragment extends Fragment {
@@ -18,20 +20,54 @@ public class InboundManualFragment extends Fragment {
         super.onAttach(context);
         mainActivity = (MainActivity)context;
     }
-
-    public InboundManualFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mainView = inflater.inflate(R.layout.fragment_inbound_manual, container, false);
 
+        final LinearLayout LinearLayout_table = (LinearLayout) mainView.findViewById(R.id.inbound_table_layout_id);
+        final LinearLayout LinearLayout_confrim = (LinearLayout) mainView.findViewById(R.id.inbound_confrim_layout_id);
+        Button inbound_manual_add_btn = (Button) mainView.findViewById(R.id.inbound_manual_add);
+        Button inbound_manual_remove_btn = (Button) mainView.findViewById(R.id.inbound_manual_remove);
+        Button inbound_manual_save_btn = (Button) mainView.findViewById(R.id.inbound_manual_save);
+        Button inbound_table_confirm_btn = (Button) mainView.findViewById(R.id.inbound_table_confirm);
+        Button inbound_table_cancel_btn = (Button) mainView.findViewById(R.id.inbound_table_cancel);
+
+        inbound_manual_add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout_table.setVisibility(View.VISIBLE);
+                LinearLayout_confrim.setVisibility(View.VISIBLE);
+                Log.d("TAG", "inbound_manual_add_btn clicked!");
+            }
+        });
+        inbound_manual_remove_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("TAG", "inbound_manual_remove_btn clicked!");
+            }
+        });
+        inbound_manual_save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG", "inbound_manual_save_btn clicked!");
+            }
+        });
+        inbound_table_confirm_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LinearLayout_table.setVisibility(View.INVISIBLE);
+                LinearLayout_confrim.setVisibility(View.INVISIBLE);
+                Log.d("TAG", "inbound_table_confirm_btn clicked!");
+            }
+        });
+        inbound_table_cancel_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LinearLayout_table.setVisibility(View.INVISIBLE);
+                LinearLayout_confrim.setVisibility(View.INVISIBLE);
+                Log.d("TAG", "inbound_table_cancel_btn clicked!");
+            }
+        });
         return mainView;
 
     }
-
 }
